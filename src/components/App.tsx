439ccf26d86ff9237a1305858fabc53d8be5ba03
Argtum <silwar40k@gmail.com>
2021-07-31 16:41:51 +0300

    Добавил отображение данных и удаление элементов списка

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-import {fetchTodos, Todo} from "../actions";
+import {fetchTodos, Todo, deleteTodo} from "../actions";
 import { StoreState } from "../reducers";
 import { connect } from "react-redux";
 
 interface AppProps {
     todos: Todo[];
-    fetchTodos(): any;
+    fetchTodos: Function;
+    deleteTodo: typeof deleteTodo;
 }
 
 export class _App extends React.Component<AppProps> {
@@ -13,9 +14,17 @@ export class _App extends React.Component<AppProps> {
       this.props.fetchTodos();
     };
 
+    onTodoClick = (id: number): void => {
+        this.props.deleteTodo(id);
+    };
+
     renderList(): JSX.Element[] {
         return this.props.todos.map((todo: Todo) => {
-            return <div key={todo.id}>{todo.title}</div>
+            return (
+              <div onClick={() => this.onTodoClick(todo.id)} key={todo.id}>
+                  {todo.title}
+              </div>
+            );
         });
     }
 
@@ -33,5 +42,5 @@ const mapStateToProps = ({ todos }: StoreState): { todos: Todo[] } => {
 
 export const App = connect(
   mapStateToProps,
-  { fetchTodos }
+  { fetchTodos, deleteTodo }
 )(_App);
